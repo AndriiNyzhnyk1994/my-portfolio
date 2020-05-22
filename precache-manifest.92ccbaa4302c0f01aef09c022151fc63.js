c2ecb825288697470356ac634996db9daa6d2113
AndriiNyzhnyk1994 <prostoandrew1994@gmail.com>
2020-05-22 18:09:29 +0300

    Updates

---
+++ precache-manifest.92ccbaa4302c0f01aef09c022151fc63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2553b2e4627076ec30f0abb6e6d3667e",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "ec9af4a37b6f04be36ca",
+    "url": "/my-portfolio/static/css/main.73d72e43.chunk.css"
+  },
+  {
+    "revision": "13b856b9e94580e97894",
+    "url": "/my-portfolio/static/js/2.4b384e6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.4b384e6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec9af4a37b6f04be36ca",
+    "url": "/my-portfolio/static/js/main.7e4ade1a.chunk.js"
+  },
+  {
+    "revision": "6d7a7337562760299d40",
+    "url": "/my-portfolio/static/js/runtime-main.00bba678.js"
+  }
+]);
